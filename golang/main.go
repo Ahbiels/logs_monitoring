6ba59a13c93eaab356c0b7a7a8832e474e3a6305
Angelo Fidelis <angelo.fidelis@avenuecode.com>
2026-05-25 17:48:36 -0300

    Refactor: adicionando o multi-stage build ao dockerfile do golang, e refatorando o código do golang para rodar na porta 777

--- golang/main.go
+++ golang/main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"sync"
 	"time"
 
@@ -14,10 +15,11 @@ import (
 )
 
 var dat map[string]any
+var date_key string
 
 func main() {
 	var (
-		ch     = make(chan []byte)
+		ch     = make(chan *loggingpb.LogEntry)
 		wgLogs sync.WaitGroup
 	)
 	inicio := time.Now()
@@ -28,56 +30,67 @@ func main() {
 	}
 	defer c.Close()
 
-	lastHour := time.Now().Add(-1 * time.Minute)
-	filter := fmt.Sprintf("timestamp >= %q", lastHour.Format(time.RFC3339))
-	req := &loggingpb.ListLogEntriesRequest{
-		ResourceNames: []string{
-			"projects/cloud-cost-tam-dev/locations/global/logScopes/tam-log-monitoring",
-		},
-		Filter:  filter,
-		OrderBy: "timestamp desc",
-	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		lastHour := time.Now().Add(-5 * time.Minute)
+		filter := fmt.Sprintf("timestamp >= %q", lastHour.Format(time.RFC3339))
+		req := &loggingpb.ListLogEntriesRequest{
+			ResourceNames: []string{
+				"projects/cloud-cost-tam-dev/locations/global/logScopes/tam-log-monitoring",
+			},
+			Filter:  filter,
+			OrderBy: "timestamp desc",
+		}
 
-	// it := c.ListLogEntries(ctx, req)
-	for resp, err := range c.ListLogEntries(ctx, req).All() {
-		wgLogs.Add(1)
-		wgLogs.Go(func() {
-			handler_func(err)
-			value, err := json.Marshal(resp)
-			handler_func(err)
-			ch <- value
-			wgLogs.Done()
-		})
-	}
+		// it := c.ListLogEntries(ctx, req)
+		for resp, err := range c.ListLogEntries(ctx, req).All() {
+			wgLogs.Add(1)
+			wgLogs.Go(func() {
+				handler_error(err)
+				handler_error(err)
+				ch <- resp
+				wgLogs.Done()
+			})
+		}
 
-	go func() {
-		wgLogs.Wait()
-		close(ch)
-	}()
+		go func() {
+			wgLogs.Wait()
+			close(ch)
+		}()
 
-	for log_serelialize := range ch {
-		Producer(log_serelialize)
-	}
+		for logs := range ch {
+			Producer(logs)
+		}
 
-	duracao := time.Since(inicio)
-	fmt.Printf("A execução levou: %s\n", duracao)
+		duracao := time.Since(inicio)
+		result := fmt.Sprintf("A execução levou: %s\n", duracao)
+
+		w.Write([]byte(result))
+	})
+
+	fmt.Println("Servidor Iniciado")
+	log.Fatal(http.ListenAndServe(":7777", nil))
+
 }
 
-func Producer(ch []byte) {
+func Producer(logs *loggingpb.LogEntry) {
+	date_key = logs.Timestamp.String()
 	producer, err := sarama.NewSyncProducer([]string{"broker:9092"}, nil)
-	handler_func(err)
+	handler_error(err)
 	defer producer.Close()
-	
+
+	log_serialized, err := json.Marshal(logs)
+
 	message := &sarama.ProducerMessage{
-		Topic: "test-topic",
-		Value: sarama.ByteEncoder(ch),
+		Topic: "topic_log_monitoring",
+		Value: sarama.ByteEncoder(log_serialized),
+		Key:   sarama.StringEncoder(date_key),
 	}
 	partition, offset, err := producer.SendMessage(message)
-	handler_func(err)
+	handler_error(err)
 	log.Printf("Message sent! Partition=%d Offset=%d\n", partition, offset)
 }
 
-func handler_func(err error) {
+func handler_error(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
